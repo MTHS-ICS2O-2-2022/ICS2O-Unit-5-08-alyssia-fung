a420ab257b79d9f8e4aa650f35ef2f59490d37cd
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-16 17:06:52 +0000

    syinc

---
+++ go_app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number1, number2, counter, answer, remainder int
+
+	// Input
+	fmt.Println("Enter the first number: ")
+	fmt.Scanln(&number1)
+	fmt.Println("Enter the second number: ")
+	fmt.Scanln(&number2)
+
+	// Process
+	counter = 0
+	remainder = number1
+	for remainder >= number2 {
+		remainder -= number2
+		counter++
+	}
+	answer = counter
+
+	// Output
+	fmt.Printf("%d / %d = %d R:%d\n", number1, number2, answer, remainder)
+	fmt.Println("\nDone.")
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  // This function divides two numbers
+
+  // input
+  const number1 = parseInt(document.getElementById("number1").value)
+  const number2 = parseInt(document.getElementById("number2").value)
+
+  // process
+  let counter = 0
+  let answer = 0
+  let remainder = number1
+  while (remainder >= number2) {
+    remainder -= number2
+    counter++
+  }
+  answer = counter
+
+  // output
+  document.getElementById("numberEntered").innerHTML =
+    number1 + " / " + number2 + " = " + answer + " R:" + remainder
+}
